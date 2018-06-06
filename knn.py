d924a772b9d212f073fcaf8549cc5e40b5d0013e
Julia <juliafsfalcao@hotmail.com>
2018-06-05 22:10:12 -0300

    updates

--- knn.py
+++ knn.py
@@ -5,60 +5,49 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-def class_transform(class_name: str):
-    if class_name == "Iris-versicolor":
-        return 0
-    elif class_name == "Iris-setosa":
-        return 1
-    elif class_name == "Iris-virginica":
-        return 2
+def species_to_int(species: str) -> int:
+    if species == "Iris-versicolor": return 0
+    elif species == "Iris-setosa": return 1
+    elif species == "Iris-virginica": return 2
 
-def class_transform_back(class_id: int):
-    if class_id == 0:
-        return "versicolor"
-    elif class_id == 1:
-        return "setosa"
-    elif class_id == 2:
-        return "virginica"
+def species_to_string(species: int) -> str:
+    if species == 0: return "Iris-versicolor"
+    elif species == 1: return "Iris-setosa"
+    elif species == 2: return "Iris-virginica"
 
-data: pd.DataFrame = pd.read_csv("iris_data.csv", names = ["sepal length", "sepal width", "petal length", "petal width", "class"])
-data["class"]: pd.Series = data["class"].apply(class_transform)
+data: pd.DataFrame = pd.read_csv("iris_data.csv", names = ["sepal length", "sepal width", "petal length", "petal width", "species"])
+
+print("DATASET SAMPLE:")
+print(data.sample(frac = 0.04).to_string())
+
+data["species"]: pd.Series = data["species"].apply(species_to_int)
 
 # split into train and test
 train: pd.DataFrame
 test: pd.DataFrame
-train, test = train_test_split(data, test_size = 0.2)
-
-# represent classes by numbers
-"""
-train.loc[train["class"] == "versicolor", "class"] = 0
-train.loc[train["class"] == "setosa", "class"] = 1
-train.loc[train["class"] == "virginica", "class"] = 2
-test.loc[train["class"] == "versicolor", "class"] = 0
-test.loc[train["class"] == "setosa", "class"] = 1
-test.loc[train["class"] == "virginica", "class"] = 2
-"""
+train, test = train_test_split(data, test_size = 0.2) # 80% training, 20% testing
 
 # training info
-X: pd.DataFrame = train.iloc[ : , 0:4] # all rows, only features (sepal and petal width and length)
-y: pd.Series = train.iloc[ : , -1] # all rows, only target (class) column, as series
+X: pd.DataFrame = train.iloc[:, 0:-1] # all rows, only features (sepal and petal width and length)
+y: pd.Series = train.iloc[:, -1] # all rows, only target (species) column, as series
 y = pd.to_numeric(y) # change type from object to int64
 
 # test info
-y_test: pd.Series = pd.to_numeric(test.iloc[ : , -1]) # target column of test dataframe, as int64
-
+y_test: pd.Series = pd.to_numeric(test.iloc[:, -1]) # target column of test dataframe, as int64
 
 # apply k-nearest neighbors algorithm
 KNN = KNeighborsClassifier()
 KNN.fit(X, y) # train
-predictions = KNN.predict(test.iloc[ : , : -1]) # predict
+predictions = KNN.predict(test.iloc[:, :-1]) # predict
+
+# print results
+print("\nKNN PREDICTION RESULTS:")
 comparison = pd.DataFrame({"test_data": y_test, "prediction": predictions}) # compare test data and predicted results
-comparison["test_data"] = comparison["test_data"].apply(class_transform_back)
-comparison["prediction"] = comparison["prediction"].apply(class_transform_back)
-incorrect = comparison[comparison.test_data != comparison.prediction]
+comparison["test_data"] = comparison["test_data"].apply(species_to_string)
+comparison["prediction"] = comparison["prediction"].apply(species_to_string)
+comparison["correct"] = comparison["test_data"] == comparison["prediction"]
+print(comparison.to_string())
 
-print(comparison)
-
-print("\nAccuracy score: ", accuracy_score(y_test, predictions))
+print("\nACCURACY SCORE: ", accuracy_score(y_test, predictions))
